f60938b565e8baa678c2b4830d5aa6102e1656f7
Kaitai <kd11@hw.ac.uk>
2019-05-22 23:14:59 +0100

    update track matrix

--- trackMat.m
+++ trackMat.m
@@ -1,4 +1,4 @@
-function [feMat] = trackMat(feElt,Ezz_s,nu_s,rho_s,Ezz_a,nu_a,rho_a,Ezz_b,nu_b,rho_b,Ezz_usl,nu_usl,rho_usl,Ezz_sl,nu_sl,rho_sl,Ezz_rp,nu_rp,rho_rp,E_r,nu_r,rho_r)
+function [feMat] = trackMat(feElt,Ezz_s,nu_s,rho_s,D_s,Ezz_a,nu_a,rho_a,D_a,Ezz_b,nu_b,rho_b,D_b,Ezz_usl,nu_usl,rho_usl,D_usl,Ezz_sl,nu_sl,rho_sl,D_sl,Ezz_rp,nu_rp,rho_rp,D_rp,E_r,nu_r,rho_r,D_r)
 
 feMat = zeros(numel(feElt(:,1)),5);
 
@@ -12,12 +12,12 @@ index5 = feElt(feElt(:,2)==5,1);
 index6 = feElt(feElt(:,2)==6,1);
 index7 = feElt(feElt(:,2)==7,1);
 
-feMat(index1,3:5) = repmat([Ezz_s,nu_s,rho_s],numel(index1),1);
-feMat(index2,3:5) = repmat([Ezz_a,nu_a,rho_a],numel(index2),1);
-feMat(index3,3:5) = repmat([Ezz_b,nu_b,rho_b],numel(index3),1);
-feMat(index4,3:5) = repmat([Ezz_usl,nu_usl,rho_usl],numel(index4),1);
-feMat(index5,3:5) = repmat([Ezz_sl,nu_sl,rho_sl],numel(index5),1);
-feMat(index6,3:5) = repmat([Ezz_rp,nu_rp,rho_rp],numel(index6),1);
-feMat(index7,3:5) = repmat([E_r,nu_r,rho_r],numel(index7),1);
+feMat(index1,3:6) = repmat([Ezz_s,nu_s,rho_s,D_s],numel(index1),1);
+feMat(index2,3:6) = repmat([Ezz_a,nu_a,rho_a,D_a],numel(index2),1);
+feMat(index3,3:6) = repmat([Ezz_b,nu_b,rho_b,D_b],numel(index3),1);
+feMat(index4,3:6) = repmat([Ezz_usl,nu_usl,rho_usl,D_usl],numel(index4),1);
+feMat(index5,3:6) = repmat([Ezz_sl,nu_sl,rho_sl,D_sl],numel(index5),1);
+feMat(index6,3:6) = repmat([Ezz_rp,nu_rp,rho_rp,D_rp],numel(index6),1);
+feMat(index7,3:6) = repmat([E_r,nu_r,rho_r,D_r],numel(index7),1);
 
 end
